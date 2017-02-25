b05148151902c2f3d112cef99bfcabbc0fdaf96b
ducbang89 <ducbang.bn1989@gmail.com>
2017-02-25 23:58:05 +0700

    change url

--- app/src/main/java/com/android/keepfocus/server/request/controllers/LoginRequestController.java
+++ app/src/main/java/com/android/keepfocus/server/request/controllers/LoginRequestController.java
@@ -28,7 +28,7 @@ import java.net.URL;
 
 public class LoginRequestController {
     //public static final String ACCOUNT_BASE_URL = "http://45.63.21.174/api/account?pRequest=";
-    public static final String ACCOUNT_BASE_URL = "http://45.63.21.174/api/account?pRequest=";
+    public static final String ACCOUNT_BASE_URL = "http://45.32.103.87/api/account?pRequest=";
     private static final int NET_READ_TIMEOUT_MILLIS = 20000;
     private static final int NET_CONNECT_TIMEOUT_MILLIS = 20000;
 
